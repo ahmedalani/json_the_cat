84d6f5a5c088ec0e4ad55eb5251a2102378d4ae4
Ahmed Alani <s3leha@gmail.com>
2020-06-21 09:37:22 -0700

    error handling, edge case of invalid name, code cleanup to breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,10 +3,13 @@ const request = require('request');
 
 const breedName = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+request(`https://ai.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
   const data = JSON.parse(body);
-
-  console.log('The description is: ', data[0].description);
-  // console.log('Body!!!: ', JSON.parse(body));
-  // console.log('Typeof Body!!!: ', typeof JSON.parse(body));
+  if (!data[0]) {
+    return console.log('breed name invalid or not found');
+  }
+  console.log(data[0].description);
 });

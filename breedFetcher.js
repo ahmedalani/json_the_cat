89eb936340e6c09e50b7493d4dc12135bddf9158
Ahmed Alani <s3leha@gmail.com>
2020-06-21 09:24:23 -0700

    breedFetcher use request to api.thecatapi and search for breed name provided from terminal args, then prints out description of the breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,12 @@
+/* eslint-disable */
 const request = require('request');
-console.log(request);
+
+const breedName = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+  const data = JSON.parse(body);
+
+  console.log('The description is: ', data[0].description);
+  // console.log('Body!!!: ', JSON.parse(body));
+  // console.log('Typeof Body!!!: ', typeof JSON.parse(body));
+});
